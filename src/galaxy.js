ddfeef5e409e3c88a3ee2305b688ed60b165268a
Brandon Eads <brandon181989@hotmail.com>
2021-09-03 10:10:05 -0700

    add code to pass fifth test. green

--- src/galaxy.js
+++ src/galaxy.js
@@ -4,5 +4,6 @@ export class GalaxyAge {
     this.mercuryAge = +(year / 0.24).toFixed(2);
     this.venusAge = +(year / 0.62).toFixed(2);
     this.marsAge = +(year / 1.88).toFixed(2);
+    this.jupiterAge = +(year / 11.86).toFixed(2);
   }
 }

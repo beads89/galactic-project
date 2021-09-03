051c4fb8eb9d2d21e0050f00f6997bc67e47ce2a
Brandon Eads <brandon181989@hotmail.com>
2021-09-03 10:31:18 -0700

    add code to pass seventh test yearsLeft. green

--- src/galaxy.js
+++ src/galaxy.js
@@ -2,6 +2,7 @@ export class GalaxyAge {
   constructor(year) {
     this.year = year;
     this.expectancy = 80;
+    this.yearsLeft = this.expectancy - this.year;
     this.mercuryAge = +(year / 0.24).toFixed(2);
     this.venusAge = +(year / 0.62).toFixed(2);
     this.marsAge = +(year / 1.88).toFixed(2);
